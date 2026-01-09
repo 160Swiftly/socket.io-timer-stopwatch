d2966c17c96617b52c3c903f7eba68c7e630dea6
Siva <siva.tharusan@gmail.com>
2026-01-08 22:16:03 -0500

    changed port in server.py

--- bin/server.py
+++ bin/server.py
@@ -121,4 +121,4 @@ def handle_close_modal():
 
 # Run the application
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
